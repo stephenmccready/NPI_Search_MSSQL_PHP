73ecdd1d49205e576cdad1a6de02c742978cce1a
Stephen McCready <stephenmccready@gmail.com>
2014-08-18 12:17:00 -0400

    Create getProviderFromNPIviaNPI.php

---
+++ php/getProviderFromNPIviaNPI.php
@@ -0,0 +1,25 @@
+<?php
+include('config.php');
+
+if( $conn === false )
+      { die( FormatErrors( sqlsrv_errors() ) ); }
+
+$tsql = "Exec AC_GetNPI_UsingNPI ? ";
+
+$params = array( $_GET['NPI'] );
+$getNPI = sqlsrv_query( $conn, $tsql, $params);
+if ( $getNPI === false)
+     { die( FormatErrors( sqlsrv_errors() ) ); }
+
+$kount=0;
+while( $row = sqlsrv_fetch_array( $getNPI, SQLSRV_FETCH_ASSOC))
+{
+	include('dispNPIData.php');
+	$kount++;
+}
+
+If($kount==0)
+{
+	echo "No providers found<br /><br />";
+	echo "NPI: ".$params[0];
+}
