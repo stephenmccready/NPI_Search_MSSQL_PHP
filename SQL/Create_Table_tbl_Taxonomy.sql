9090854852a45de4edfff57150b3ebe70db9e1fb
Stephen McCready <stephenmccready@gmail.com>
2014-08-18 12:27:58 -0400

    Create Create_Table_tbl_Taxonomy.sql

---
+++ SQL/Create_Table_tbl_Taxonomy.sql
@@ -0,0 +1,18 @@
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+SET ANSI_PADDING ON
+GO
+
+CREATE TABLE [dbo].[tbl_Taxonomy](
+	[TaxonomyDescription] [varchar](256) NULL,
+	[TaxonomyCode] [varchar](32) NULL
+) ON [PRIMARY]
+
+GO
+
+SET ANSI_PADDING OFF
+GO
